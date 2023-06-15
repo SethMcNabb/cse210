7a7005fad87077944fc902c767d237b57d631b4f
SethMcNabb <youremail@byui.edu>
2023-06-15 08:49:44 -0600

    Finished resume program.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Reliaquest";
+        job1._jobTitle = "Product Manager";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Conservice";
+        job2._jobTitle = "Associate Software Engineer";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jhan-Jhan";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
